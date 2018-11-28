553a626a67f08b7cc619943f6c1557332c1c9446
Mike McGuire <mike@mikejmcguire.com>
2018-11-29 00:28:30 +1100

    Added instrumentation

---
+++ hass-actron/Controllers/Instrumentation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMX.HASSActron.Controllers
+{
+	public class Instrumentation : Controller
+	{
+		[Route("/")]
+		public IActionResult Test()
+		{
+			ContentResult result = new ContentResult();
+
+			result.ContentType = "text/html";
+			result.StatusCode = 200;
+
+			result.Content = "OK";
+
+			return result;
+		}
+	}
+}
