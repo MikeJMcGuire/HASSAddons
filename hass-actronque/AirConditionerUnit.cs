03308c9098fc213659f926f0ac6f870ce5e6b54d
Mike McGuire <mike@mcguire.mx>
2023-01-05 04:18:15 +0000

    Squashed 'hass-actronque/' changes from 5a95742..a9d2b0e
    a9d2b0e Final
    c7cb5e4 Updates
    4a10891 Updates
    bb267b9 Updates
    6677d21 Updates
    f52cf86 Updates
    33f1205 Updates
    4bbd99b Updates
    3721217 Updates
    3c2752a Updates
    383447d Updates
    f510ee7 Updates
    a56e85a Updates
    6d1b96b Ready for Testing
    6f3f88e Updates
    6a61839 Updates
    31cf56a v2023.1.0 Initial
    
    git-subtree-dir: hass-actronque
    git-subtree-split: a9d2b0e68618e6674b13a58baad9ce166645f82b

---
+++ hass-actronque/AirConditionerUnit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMX.HASSActronQue
+{
+	public class AirConditionerUnit
+	{
+		public string Name;
+		public string NextEventURL;
+		public string Serial;
+		public AirConditionerData Data;
+		public Dictionary<int, AirConditionerZone> Zones;		
+
+		public AirConditionerUnit(string strName, string strSerial)
+		{
+			Name = strName;
+			Serial = strSerial;
+			NextEventURL = "";
+			Data = new AirConditionerData();
+			Zones = new Dictionary<int, AirConditionerZone>();
+		}
+	}
+}
--- hass-actronque/QueueCommand.cs
+++ hass-actronque/QueueCommand.cs
@@ -9,10 +9,12 @@ namespace HMX.HASSActronQue
 		public long RequestId;
 		public QueueCommandData Data;
 		public DateTime Expires;
+		public AirConditionerUnit Unit;
 
-		public QueueCommand(long lRequestId, DateTime dtExpires)
+		public QueueCommand(long lRequestId, AirConditionerUnit unit, DateTime dtExpires)
 		{
 			RequestId = lRequestId;
+			Unit = unit;
 			Data = new QueueCommandData();
 			Expires = dtExpires;
 		}
